90041e6f1d76e6ee7984418e14a71df600145441
wnkwnl <122200098+wnkwnl@users.noreply.github.com>
2023-05-29 18:58:52 +0530

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,44 @@
+Status = "";
+bottle_image = "";
+objects = [];
+
+function preload(){
+    bottle_image = loadImage("bottle.JPG");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(bottle_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(bottle_image,0,0,640,350);
+    if(Status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#fc0303");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x - 800, objects[i].y - 520);
+            noFill();
+            stroke("#fc0303");
+            rect(objects[i].x - 800, objects[i].y - 520, objects[i].width - 910, objects[i].height - 2640);
+        }
+    }
+}
---
+++ bulb.js
@@ -0,0 +1,44 @@
+Status = "";
+bulb_image = "";
+objects = [];
+
+function preload(){
+    bulb_image = loadImage("bulb.JPG");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(bulb_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(bulb_image,0,0,640,350);
+    if(Status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#fc0303");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x, objects[i].y);
+            noFill();
+            stroke("#fc0303");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
